7b8fbd7c173b81e0a15182e83f68be2b7d434e1f
Petr Gladkikh <PetrGlad@gmail.com>
2011-10-13 21:05:01 +0700

    Better call confirmation.

--- src/petrglad/callpad/CallPadActivity.java
+++ src/petrglad/callpad/CallPadActivity.java
@@ -1,18 +1,16 @@
 package petrglad.callpad;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.CallLog;
 import android.util.Log;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -33,45 +31,30 @@ public class CallPadActivity extends ListActivity {
 
         listAdapter = new CallListAdapter(this, query);
 
-        this.setListAdapter(listAdapter);
-
-        // SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
-        // Collection<String> listItems = new ArrayList<String>();
-        // for (Sensor s : sm.getSensorList(Sensor.TYPE_ALL))
-        // listItems.add(s.getName() + " (" + s.getType() + ")");
-        // setListAdapter(new ArrayAdapter<String>(this,
-        // android.R.layout.simple_list_item_1,
-        // listItems.toArray(new String[listItems.size()])));
-
+        setListAdapter(listAdapter);
         registerForContextMenu(getListView());
     }
 
     @Override
-    protected void onListItemClick(ListView l, View v, int position, long id) {
+    protected void onListItemClick(ListView l, View v, int position, final long id) {
         Log.v(CallPadActivity.class.getName(), "pos=" + position + ", od=" + id
                 + ", data=" + getListView().getItemAtPosition(position));
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
-        if (v.getId() == android.R.id.list) {
-            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-            int menuPos = 0;
-            CallItem callItem = (CallItem) listAdapter.getItem(info.position);
-            final String phoneNo = callItem.phoneNo;
-            menu.setHeaderTitle(phoneNo);
-            menu.add(Menu.NONE, menuPos, menuPos, "Call").setOnMenuItemClickListener(
-                    new MenuItem.OnMenuItemClickListener() {
-                        @Override
-                        public boolean onMenuItemClick(MenuItem item) {
-                            Log.v(getClass().getName(), "call clicked " + item.getItemId());
-                            startActivity(new Intent(
-                                    "android.intent.action.DIAL",
-                                    Uri.fromParts("tel", phoneNo, "")));
-                            return false;
-                        }
-                    });
-            menu.add(Menu.NONE, menuPos, menuPos, "Dismiss");
-        }
+        final CallItem callItem = (CallItem) l.getAdapter().getItem(position);
+        final String phoneNo = callItem.phoneNo;
+        new AlertDialog.Builder(this)
+                .setTitle("Confirm call")
+                .setMessage("Call " + phoneNo)
+                .setPositiveButton("Call", new OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.v(getClass().getName(), "call clicked " + id + ", phoneNo " + phoneNo);
+                        startActivity(new Intent(
+                                "android.intent.action.DIAL",
+                                Uri.fromParts("tel", phoneNo, "")));
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
